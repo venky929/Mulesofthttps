5bedcb290ecb883394821aa0630aa7fd97c87135
Venkatesh Yedururu <yvenkatesh929@gmail.com>
2021-06-24 16:17:46 +0530

    invoke java project

---
+++ invokejavamethod/src/main/java/invokejavamethod/Stringoperations.java
@@ -0,0 +1,16 @@
+package invokejavamethod;
+
+public class Stringoperations {
+	
+	public static String concat(String a, String b) 
+	{
+		
+		return a+" "+b;
+	}
+	
+	public String multiconcat(String a,String b,String c)
+	{
+		return a+ " "+b+" "+c;
+	}
+
+}
